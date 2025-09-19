e7a8fb128c2a586e1c2eacdfde27c9ca810e8824
matheusqueiroz92 <matheus_giga@hotmail.com>
2025-09-18 21:46:47 -0300

    feat: change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ import { Mail, MapPin } from "lucide-react";
 import { WhatsAppIcon } from "./ui/whatsapp-icon";
 import { trackButtonClick } from "@/config/gtm";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Footer() {
   return (
@@ -13,12 +14,14 @@ export function Footer() {
           <div className="flex flex-col lg:flex-row items-center justify-center gap-12">
             {/* Logo e Nome da Empresa */}
             <div className="flex items-center gap-4">
-              <Image
-                src="/logo-rodape.png"
-                alt="Logo Suzy Brito Advocacia"
-                width={300}
-                height={100}
-              />
+              <Link href="#inicio">
+                <Image
+                  src="/logo-rodape.png"
+                  alt="Logo Suzy Brito Advocacia"
+                  width={300}
+                  height={100}
+                />
+              </Link>
             </div>
 
             {/* Linha vertical separadora */}
@@ -28,30 +31,51 @@ export function Footer() {
             <div className="space-y-4 min-w-0 flex-1 max-w-md">
               {/* WhatsApp/Telefone */}
               <div className="flex items-center gap-3">
-                <div className="w-5 h-5 text-white flex-shrink-0">
-                  <WhatsAppIcon />
-                </div>
-                <span className="text-white font-medium whitespace-nowrap">
-                  (77) 99111-2894
-                </span>
+                <Link
+                  href="https://wa.me/5577991112894?text=Ol%C3%A1%2C%20Suzy%20Brito%20Advocacia!%20Visitei%20o%20site%20e%20gostaria%20de%20conversar%20sobre%20busca%20e%20apreens%C3%A3o.%20Poderiam%20me%20ajudar%3F"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex items-center gap-3">
+                    <div className="w-5 h-5 text-white flex-shrink-0">
+                      <WhatsAppIcon />
+                    </div>
+                    <span className="text-white font-medium whitespace-nowrap">
+                      (77) 99111-2894
+                    </span>
+                  </div>
+                </Link>
               </div>
 
               {/* Email */}
               <div className="flex items-center gap-3">
-                <Mail className="w-5 h-5 text-white flex-shrink-0" />
-                <span className="text-white font-medium whitespace-nowrap">
-                  contato@suzibrito.adv.br
-                </span>
+                <Link href="mailto:suzibrito.adv@gmail.com">
+                  <div className="flex items-center gap-3">
+                    <Mail className="w-5 h-5 text-white flex-shrink-0" />
+                    <span className="text-white font-medium whitespace-nowrap">
+                      suzibrito.adv@gmail.com
+                    </span>
+                  </div>
+                </Link>
               </div>
 
               {/* Endereço */}
               <div className="flex items-start gap-3">
-                <MapPin className="w-5 h-5 text-white mt-0.5 flex-shrink-0" />
-                <div className="text-white font-medium">
-                  <p className="whitespace-nowrap">
-                    Rua da Liberdade, 123 Centro, Salvador/BA CEP: 40000-000
-                  </p>
-                </div>
+                <Link
+                  href="https://maps.app.goo.gl/4V73cQehhCYGo1v39"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex items-start gap-3">
+                    <MapPin className="w-5 h-5 text-white mt-0.5 flex-shrink-0" />
+                    <div className="text-white font-medium">
+                      <p className="whitespace-nowrap">
+                        Rua 7 de setembro, nº 29, Centro. Itapetinga-BA, CEP:
+                        45700-000
+                      </p>
+                    </div>
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
